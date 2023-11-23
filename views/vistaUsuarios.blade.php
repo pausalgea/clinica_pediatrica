e8cc083d1bf20816750a3686d687a4cd803ad2d0
pausalgea <pausaalgea@gmail.com>
2023-11-23 18:33:15 +0100

    Update vistaUsuarios.blade.php

--- views/vistaUsuarios.blade.php
+++ views/vistaUsuarios.blade.php
@@ -15,7 +15,6 @@
 </script>
 @endsection
 @section('contenido')
-<<<<<<< HEAD
 <div class="float float-right d-inline-flex mt-4">
     
     <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
@@ -27,7 +26,6 @@
 
 </div>
 <br>
-=======
 <div class="float float-right d-inline-flex mt-2">
 
     <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
@@ -37,7 +35,6 @@
     <a style="float:right" class="btn btn-secondary" href="logout.php" role="button">Cerrar sesión</a>
 
 </div>
->>>>>>> f9b92fbb1822d02bfb5604049a934146d08a26df
 
 
 @if($tipo=="administrador")
@@ -101,19 +98,13 @@
                 Gestionar consultas</a>
         </td>
         <td>
-<<<<<<< HEAD
             <a class="btn btn-success" href="gestionPaciente.php" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-vcard-fill" viewBox="0 0 16 16">
-=======
             <a class="btn btn-info" href="gestionPaciente.php" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-vcard-fill" viewBox="0 0 16 16">
->>>>>>> f9b92fbb1822d02bfb5604049a934146d08a26df
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm9 1.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4a.5.5 0 0 0-.5.5ZM9 8a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4A.5.5 0 0 0 9 8Zm1 2.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5Zm-1 2C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 0 2 13h6.96c.026-.163.04-.33.04-.5ZM7 6a2 2 0 1 0-4 0 2 2 0 0 0 4 0Z" />
 
                 </svg> Gestionar pacientes</a>
         </td>
-<<<<<<< HEAD
 
-=======
->>>>>>> f9b92fbb1822d02bfb5604049a934146d08a26df
     </tr>
 </table>
 </div>
@@ -123,11 +114,8 @@
 <table class="table table-striped">
     <thead>
         <tr class="text-center">
-<<<<<<< HEAD
             <th scope="col">Id consulta</th>
-=======
             <th scope="col">Id</th>
->>>>>>> f9b92fbb1822d02bfb5604049a934146d08a26df
             <th scope="col">Fecha</th>
             <th scope="col">Hora</th>
             <th scope="col">Login médico</th>
@@ -144,17 +132,16 @@
             <td>{{$item->login_medico}}</td>
             <td>{{$item->DNI}}</td>
 
-<<<<<<< HEAD
             <td><a class="btn btn-dark" href="generarJustificante.php?id_consulta={{$item->id_consulta}}&login={{$nombre}}&DNI={{$item->DNI}}" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf-fill" viewBox="0 0 16 16">
                 <path d="M5.523 12.424c.14-.082.293-.162.459-.238a7.878 7.878 0 0 1-.45.606c-.28.337-.498.516-.635.572a.266.266 0 0 1-.035.012.282.282 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548zm2.455-1.647c-.119.025-.237.05-.356.078a21.148 21.148 0 0 0 .5-1.05 12.045 12.045 0 0 0 .51.858c-.217.032-.436.07-.654.114zm2.525.939a3.881 3.881 0 0 1-.435-.41c.228.005.434.022.612.054.317.057.466.147.518.209a.095.095 0 0 1 .026.064.436.436 0 0 1-.06.2.307.307 0 0 1-.094.124.107.107 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256zM8.278 6.97c-.04.244-.108.524-.2.829a4.86 4.86 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.517.517 0 0 1 .145-.04c.013.03.028.092.032.198.005.122-.007.277-.038.465z"/>
                 <path fill-rule="evenodd" d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3zM4.165 13.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.651 11.651 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.856.856 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.844.844 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.76 5.76 0 0 0-1.335-.05 10.954 10.954 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.238 1.238 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a19.697 19.697 0 0 1-1.062 2.227 7.662 7.662 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103z"/>
               </svg> Generar justificante</a>
-=======
+
             <td><a class="btn btn-danger" href="generarFactura?id_consulta={{$item->id_consulta}}" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
                     </svg>Generar justificante</a>
->>>>>>> f9b92fbb1822d02bfb5604049a934146d08a26df
+
             </td>
         </tr>
         @endforeach
